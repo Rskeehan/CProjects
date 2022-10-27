ae23b4b7201ce47c5928651509f6cedd8f549c9d
rob <rskeehan@gmail.com>
2022-10-27 10:50:53 -0500

    moving files around

--- CForEveryoneTraining_A/MiscelaneousPrograms/add3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(void){
-    float a, b, c, sum;
-    printf("Input 3 Floats: ");
-    scanf("%f%f%f", &a,&b,&c);
-    sum = a+b+c;
-    printf("The sum of your floats is: %f",sum);
-}
--- CForEveryoneTraining_A/Week3/Week3Sandbox.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main (int argc, char *argv[]) {
-    int i, j, n;
-    i=1;j=2;
-    n=i,j+1;
-    printf("%i",n);
-    return 0;
-}
